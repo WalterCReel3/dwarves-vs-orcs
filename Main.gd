36e0f46eab8b5eff354c78067c98c17f0f387f69
Walter Reel <waltercreel3rd@gmail.com>
2021-03-17 13:57:14 -0400

    Click to move

--- Main.gd
+++ Main.gd
@@ -6,21 +6,19 @@ extends Node2D
 # var b = "text"
 
 
+onready var selected_actor = get_node('Actor')
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	print("Viewport Resolution is: ", get_viewport_rect().size)
 	pass # Replace with function body.
 
 func _input(event):
 	# Mouse in viewport coordinates.
 	if event is InputEventMouseButton:
+		# selected_actor.x = event.position.x
 		print("Mouse Click/Unclick at: ", event.position)
-		print("Viewport Resolution is: ", get_viewport_rect().size)
-
-   # elif event is InputEventMouseMotion:
-   #     print("Mouse Motion at: ", event.position)
-
-   # Print the size of the viewport.
-   # 
+		selected_actor.set_target(event.position)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- entities/Actor.gd
+++ entities/Actor.gd
@@ -2,13 +2,15 @@ extends KinematicBody2D
 
 var speed = 200
 var velocity = Vector2()
+var target = Vector2()
 
 const IDLE = 'Idle'
 const HIGH_ATTACK = 'HighAttack'
+const SNAP = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	target = self.position
 	
 func get_input():
 	if Input.is_action_pressed("move_left"):
@@ -21,7 +23,23 @@ func get_input():
 	if velocity.x < 0:
 		velocity.x += 50
 
+
+func process_pathing(delta):
+	if abs(self.position.x - target.x) < SNAP:
+		velocity.x = 0
+		self.position.x = target.x
+	else:
+		if self.position.x < target.x:
+			velocity.x = speed
+		elif self.position.x > target.x:
+			velocity.x = -speed
+		move_and_collide(velocity * delta)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	get_input()
-	move_and_collide(velocity * delta)
+	# get_input()
+	process_pathing(delta)
+
+
+func set_target(position):
+	target = position
